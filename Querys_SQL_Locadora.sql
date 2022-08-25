7453811d90ab43ec5864e3ad862381f8afc6afb7
nicolemorais <99222688+nicolemorais@users.noreply.github.com>
2022-08-25 17:29:55 -0300

    Update Querys_SQL_Locadora.sql

--- Querys_SQL_Locadora.sql
+++ Querys_SQL_Locadora.sql
@@ -12,7 +12,7 @@ SELECT
 FROM
 	actor
 ORDER BY 
-	first_name ASC;
+	first_name;
 
 -- #2 Classificando os títulos em ordem ascendente
 SELECT 
@@ -20,7 +20,7 @@ SELECT
 FROM
 	film_list
 ORDER BY 
-	title ASC;
+	title;
 
 -- #3 Classificando a categoria em ordem descendente;
 SELECT 
@@ -37,17 +37,15 @@ SELECT
 FROM
 	actor
 ORDER BY 
-	last_name ASC;
+	last_name ;
 
--- #5 Calculando o subtotal do custo de reposição e o valor dos alugueis dos filmes e ordernando em forma descentente
+-- #5 Calculando o subtotal do custo de reposição e o valor dos alugueis dos filmes
 SELECT
-	replacement_cost, 
+    replacement_cost,
     price,
-    replacement_cost * price AS subtotal
+    replacement_cost * price as 'subtotal'
 FROM
-	film_list, film
-ORDER BY 
-	replacement_cost * price DESC;
+	film_list, film;
 
 # FILTRANDO OS DADOS
 
@@ -59,7 +57,7 @@ FROM
 WHERE
     language_id;
 
--- #7 Filtrando e ordenando os títulos dos filmes por preço
+-- #7 Filtrando e ordenando os títulos dos filmes por preço determinado
 
 SELECT 
     title,
@@ -69,7 +67,7 @@ FROM
 WHERE
     price = '4.99'
 ORDER BY 
-	title ASC;
+	title ;
 
 -- #8 Filtrando atores por ID entre 1 e 23
 SELECT 
@@ -149,7 +147,7 @@ SELECT
 SELECT 
     title AS TÍTULO, 
     rating AS CLASSIFICAÇÃO,
-    SUM(price) PREÇO  
+    SUM(price) as PREÇO  
 FROM
     film_list
 GROUP BY 
@@ -167,33 +165,124 @@ FROM
 WHERE
     amount = (SELECT MAX(amount) FROM payment);
 
--- #16
+-- #16 alteração do nome da coluna name da tabela language para languages_name
+ALTER TABLE language
+CHANGE name languages_name char(20);
 
--- #17 
+-- #17 deletado um dado da tabela pelo id correspondente
+DELETE FROM inventory WHERE inventory_id = '5';
 
--- #18
+-- #18 alteração no valor de pagamento
+UPDATE  
+	payment
+SET 
+	amount = '34.99' 
+WHERE 
+	rental_id ='670';
 
--- #19
+-- #19 União de valores menores que 2 e maiores que 3
+SELECT 
+	payment_id, amount
+FROM 
+	payment
+where 
+	amount <='2.00'
+UNION	SELECT 
+	payment_id, amount
+FROM 
+	payment
+where
+	amount >= '3.00';
 
--- #20
+-- #20 mudando o nome da coluna
+ALTER TABLE 
+	actor
+CHANGE 
+	last_name sobrenome char(20);
 
--- #21
+-- #21 mudando o nome do cliente
+UPDATE  
+	customers
+SET 
+	first_name = 'Heloisa' 
+WHERE 
+	rental_id ='15';
 
--- #22
-
--- #23
-
--- #24
-
--- #25
-
--- #26
-
--- #27 
-
--- #28
+-- #22 acrescentando uma coluna nova na tabela
+ALTER TABLE 
+	payment
+ADD 
+	forma_pagamento varchar(20);
+    
+-- #23 inserindo valores a coluna nova
+UPDATE  
+	payment
+SET 
+	forma_pagamento = 'Cartão de Credito' 
+WHERE 
+	payment_id <30;
+    
+-- #24 terminando de inserir valor a coluna
+UPDATE  
+	payment
+SET 
+	forma_pagamento = 'Dinheiro' 
+WHERE 
+	payment_id >70;
+    
+-- #25  excluindo coluna criada
+ALTER TABLE 
+	payment
+DROP COLUMN 
+	forma_pagamento;
+    
+-- #26 CROSS JOIN entre cidades e paises
+SELECT 
+    c.city_id,
+    c.city,
+    y.country_id,
+    y.country
+FROM
+    city c
+CROSS JOIN country y ON 
+    c.country_id = y.country_id;
+    
+-- #27 filtro de filmes de acordo com limite de 6 itens em estoque
+SELECT
+	inventory_id,
+    last_update
+FROM
+  inventory
+WHERE
+	film_id between 4 and 10
+    ORDER BY inventory_id
+    limit 6;
+    
+-- #28 	Mudando nome da tabela
+ALTER TABLE film
+CHANGE  title title_film char(255);
 
 -- #29
-
--- #30
-
+SELECT 
+	first_name,
+    last_name
+FROM
+	customer
+where active = 'true'
+ORDER BY 
+	first_name DESC;
+    
+-- #30 clientes que alugaram com valor maior de 10
+SELECT 
+	p.customer_id,
+    c.first_name,
+    c.last_name,
+    amount
+FROM
+	payment p
+    inner join customer c
+    on
+    p.customer_id = c.customer_id
+where amount > '10.00'
+ORDER BY 
+	first_name;
